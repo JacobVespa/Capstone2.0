d774e5f4aa036991308e24fc968691fbccbe522d
KenanButler <kenan.butler909@gmail.com>
2025-11-18 17:53:16 -0500

    scene switch implementation

---
+++ Cave_Crashers01/Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes : MonoBehaviour
+{
+    [SerializeField] string sceneName;
+    [SerializeField] int sceneIndex;
+    [SerializeField] Collider collider;
+
+    public void LoadSceneByName()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadSceneByIndex()
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("lol");
+            LoadSceneByName();
+        }
+    }
+}
