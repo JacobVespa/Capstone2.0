efb5cec5043e328ba3f9731035db58d23b259bb5
Stephen R <160242773+Stephentester@users.noreply.github.com>
2026-01-08 18:16:29 -0500

    Revert "Ammo Box SFX"
    This reverts commit 3e3efee986fb06d9ab944b2a63e69fe6541d6b24.

--- 3.0Capstone/Assets/Scripts2.0/AmmoBox.cs
+++ 3.0Capstone/Assets/Scripts2.0/AmmoBox.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 
 public class AmmoBox : MonoBehaviour
 {
-    [SerializeField] AudioClip ammoClip;
-    [SerializeField] AudioSource audioSource;
+
     //reference to the ammo round
     public GameObject ammoRound;
 
     //instantiating the ammo round
     public void SpawnAmmo()
     {
-        audioSource.Play();
         Instantiate(ammoRound);
     }
 
