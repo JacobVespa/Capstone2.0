b5f5b07d84e197689960c2b0cc2d007c0909bcef
Stephen R <160242773+Stephentester@users.noreply.github.com>
2026-01-08 18:30:22 -0500

    Ammobox sfx

--- 3.0Capstone/Assets/Scripts2.0/AmmoBox.cs
+++ 3.0Capstone/Assets/Scripts2.0/AmmoBox.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class AmmoBox : MonoBehaviour
 {
-
+    [SerializeField] AudioClip ammoBox;
+    [SerializeField] AudioSource audioSource;
     //reference to the ammo round
     public GameObject ammoRound;
 
     //instantiating the ammo round
     public void SpawnAmmo()
     {
+        audioSource.Play();
         Instantiate(ammoRound);
     }
 
