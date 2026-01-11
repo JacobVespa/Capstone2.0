f7066c3367fc43be9119d1bedc387628092c497d
Absurd24 <160165230+Absurd24@users.noreply.github.com>
2026-01-11 14:39:37 -0500

    added new sprites

--- 3.0Capstone/Assets/Scripts2.0/WallMoving.cs
+++ 3.0Capstone/Assets/Scripts2.0/WallMoving.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class WallMoving : MonoBehaviour
 {
+    [SerializeField] public float wallMoveSpeed = 3;
+
 
     // Update is called once per frame
     void Update()
     {
-        transform.position -= new Vector3(0, 3, 0) * Time.deltaTime;
+        transform.position -= new Vector3(0, wallMoveSpeed, 0) * Time.deltaTime;
     }
 
     private void OnTriggerEnter(Collider other)
