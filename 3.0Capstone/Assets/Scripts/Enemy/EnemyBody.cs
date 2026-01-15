a7d527a98fa7095b7ca751855ba25182eb7a5aba
Eliogeno <eliogbp@gmail.com>
2026-01-15 14:44:05 -0500

    Grub death anim added
    Also added floor scrolling

--- 3.0Capstone/Assets/Scripts/Enemy/EnemyBody.cs
+++ 3.0Capstone/Assets/Scripts/Enemy/EnemyBody.cs
@@ -57,7 +57,8 @@ public class EnemyBody : MonoBehaviour, IDamageReceiver
     protected float attackCooldown = 0;
     public float AttackCooldown { get { return attackCooldown; } set { attackCooldown = value; } }
 
-    
+    [Header("Animator")]
+    [SerializeField] private Animator enemyAnims;
 
     
 
@@ -118,8 +119,16 @@ public class EnemyBody : MonoBehaviour, IDamageReceiver
         health -= damage;
         if(health <= 0)
         {
-            Death();
+            StartCoroutine(PlayDeathAnim());
         }
+    }
+
+    private IEnumerator PlayDeathAnim()
+    {
+        enemyAnims.SetBool("Death", true); //MAKE DEATH ANIM PARAMETER THE SAME NAME FOR ALL ENEMIES
+        yield return new WaitForSeconds(1);
+        enemyAnims.SetBool("Death", false);
+        Death();
     }
 
     private void Death()
