3166bd09b8d8746cabd7f8e0edccb95830d9d7e6
Eliogeno <eliogbp@gmail.com>
2026-01-15 15:01:55 -0500

    skeeto anims set up in code

--- 3.0Capstone/Assets/Scripts/Enemy/EnemyBody.cs
+++ 3.0Capstone/Assets/Scripts/Enemy/EnemyBody.cs
@@ -125,15 +125,17 @@ public class EnemyBody : MonoBehaviour, IDamageReceiver
 
     private IEnumerator PlayDeathAnim()
     {
-        enemyAnims.SetBool("Death", true); //MAKE DEATH ANIM PARAMETER THE SAME NAME FOR ALL ENEMIES
-        yield return new WaitForSeconds(1);
-        enemyAnims.SetBool("Death", false);
+        if(enemyAnims != null) //if enemy has animations, play them before triggering death
+        {
+            enemyAnims.SetBool("Death", true); //MAKE DEATH ANIM PARAMETER THE SAME NAME FOR ALL ENEMIES
+            yield return new WaitForSeconds(1);
+            enemyAnims.SetBool("Death", false);
+        }
         Death();
     }
 
     private void Death()
     {
-        
         ai.behaviour = EnemyAI.Behaviour.Dead;
         sprite.SetActive(false);
         Collider2D[] colliders = GetComponents<Collider2D>();
