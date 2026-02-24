1d671a3ef043b7a7a65cbf57cce1bc000b154be1
JacobVespa <vespajacob@gmail.com>
2026-02-24 15:47:50 -0500

    Layer Fixes and Bug Fixes
    Many more layer fixes needed!

--- 3.0Capstone/Assets/Scripts2.0/Menus/ResultMenu_UI.cs
+++ 3.0Capstone/Assets/Scripts2.0/Menus/ResultMenu_UI.cs
@@ -37,21 +37,23 @@ public class ResultMenu_UI : MonoBehaviour
             resultText.text = winMessage;
         }
 
-        //Count from zero slowly and ramp up speed
-
         totalShards = GameManager.Instance.Shards;
         totalKills = GameManager.Instance.Kills;
         CountUpScore();
     }
 
-    private void CountUpScore()
-    {
-        StartCoroutine(Counter());
-    }
-
     private float multiplier = 1.0f;
     private int tempShards = 0;
     private int tempKills = 0;
+
+    private void CountUpScore()
+    {
+        StopAllCoroutines();
+        tempShards = 0;
+        tempKills = 0;
+        multiplier = 1.0f;
+        StartCoroutine(Counter());
+    }
 
     private IEnumerator Counter()
     {
@@ -59,15 +61,17 @@ public class ResultMenu_UI : MonoBehaviour
         {
             if (tempShards < totalShards)
             {
-                shardsText.text = "Shards: " + (tempShards++).ToString();
+                tempShards++;
+                shardsText.text = "Shards: " + tempShards.ToString();
             }
             if (tempKills < totalKills)
             {
-                killsText.text = "Kills: " + (tempKills++).ToString();
+                tempKills++;
+                killsText.text = "Kills: " + tempKills.ToString();
             }
 
             yield return new WaitForSecondsRealtime(0.4f * multiplier);
-            multiplier -= 0.05f;
+            multiplier = Mathf.Max(0.05f, multiplier - 0.05f);
         }
     }
 
