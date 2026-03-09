d18f07e90adec9fce4857c8de129cbb3999753fb
KenanButler <kenan.butler909@gmail.com>
2026-03-09 00:38:51 -0400

    adjusting hitboxes

--- 3.0Capstone/Assets/Scripts2.0/RIG/Bullet.cs
+++ 3.0Capstone/Assets/Scripts2.0/RIG/Bullet.cs
@@ -15,9 +15,9 @@ public class Bullet : MonoBehaviour
     {
         if(collision.CompareTag("Enemy"))
         {
-            Debug.LogError("Hit Enemy with a bullet");
+            //Debug.LogError("Hit Enemy with a bullet");
             turret.HitEnemy(collision);
-            //Destroy(gameObject);
+            Destroy(gameObject);
         }
         if(collision.CompareTag("Gem"))
         {
@@ -33,7 +33,7 @@ public class Bullet : MonoBehaviour
         {
             Debug.Log("BRUH");
             turret.HitGrass(collision);
-            Destroy(gameObject);
+            //Destroy(gameObject);
         }
     }
 }
