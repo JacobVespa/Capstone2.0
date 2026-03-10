7a616c1fd7c813a4b7366e71c62bd47c9022a1a5
samversion1 <90704185+samversion1@users.noreply.github.com>
2026-03-10 18:20:32 -0400

    Added Tread Scrolling

---
+++ 3.0Capstone/Assets/Scripts2.0/Environment/TreadMoving.cs
@@ -0,0 +1,102 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TreadMoving : MonoBehaviour
+{
+    [SerializeField] private GameObject[] TreadSprites;
+    [SerializeField] public float TreadMoveSpeed;
+    [SerializeField] private bool isLooping = true;
+
+    private bool hasFinished = false;
+    private bool isPaused = false;
+
+    private Vector3 startPosition;
+
+    //offset wall and floor position for despawning
+    private float TreadOffsetPos = -6f;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        
+    }
+
+    void Update()
+    {
+        if (isPaused) return;
+
+        if (isLooping)
+        {
+            TreadMovement();
+        }
+        else if (!hasFinished)
+        {
+            TreadMovement();
+
+            if (gameObject.CompareTag("Tread"))
+            {
+                HasFinishedCheck(TreadOffsetPos);
+            }
+        }
+    }
+
+    private void TreadMovement()
+    {
+        if (gameObject.CompareTag("Tread"))
+        {
+            transform.position -= new Vector3(0, TreadMoveSpeed, 0) * Time.deltaTime;
+            DistanceCheck(TreadOffsetPos, 7f);
+        }
+    }
+
+    private void DistanceCheck(float offsetPos, float yPos)
+    {
+        if (isLooping && transform.position.y <= offsetPos)
+        {
+            transform.position = new Vector3(0, yPos, 0);
+        }
+    }
+
+    private void HasFinishedCheck(float offsetPos)
+    {
+        float distanceTraveled = startPosition.y - transform.position.y;
+        if (distanceTraveled >= Mathf.Abs(offsetPos))
+        {
+            hasFinished = true;
+        }
+    }
+
+    public void SetLooping(bool looping)
+    {
+        isLooping = looping;
+
+        if (!looping)
+        {
+            hasFinished = false;
+            startPosition = transform.position;
+        }
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+    }
+
+    public void Play()
+    {
+        isPaused = false;
+    }
+
+    public float GetTreadSpeed()
+    {
+        if(isPaused)
+        {
+            return 0f;
+        }
+        else
+        {
+            return TreadMoveSpeed;
+        }
+    }
+
+}
