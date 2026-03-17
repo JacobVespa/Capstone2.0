de665b38b60ea510b318dfc2000d2b110fb183a1
Eliogeno <eliogbp@gmail.com>
2026-03-16 20:36:33 -0400

    Question mark room progress

---
+++ 3.0Capstone/Assets/Scripts2.0/Managers/QVariants/QVariant1Manager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class QVariant1Manager : MonoBehaviour
+{
+    
+    public void Option1Press()
+    {
+        Debug.Log("LEFT BUTTON PRESSED");
+    }
+
+    public void Option2Press()
+    {
+        Debug.Log("RIGHT BUTTON PRESSED");
+    }
+
+}
