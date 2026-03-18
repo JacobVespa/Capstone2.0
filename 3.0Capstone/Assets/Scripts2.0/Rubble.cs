2617d339df11c546a407f3dd908eed33566e3b50
Absurd24 <160165230+Absurd24@users.noreply.github.com>
2026-03-17 20:12:52 -0400

    a buncha stuff

---
+++ 3.0Capstone/Assets/Scripts2.0/Rubble.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Rubble : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //private void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    Debug.Log("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+    //    if (other.gameObject.CompareTag("Rig"))
+    //    {
+    //        GetComponentInChildren<SpriteRenderer>().enabled = false;
+    //        GetComponentInChildren<ParticleSystem>().Play();
+    //    }
+    //}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+        if (collision.gameObject.CompareTag("Rig"))
+        {
+            GetComponentInChildren<SpriteRenderer>().enabled = false;
+            GetComponentInChildren<ParticleSystem>().Play();
+        }
+    }
+
+
+}
