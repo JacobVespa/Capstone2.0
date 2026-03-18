bfed2c1a46dcb04b123375b6dede30f973a7b8a6
Eliogeno <eliogbp@gmail.com>
2026-03-17 20:24:04 -0400

    Cool mystery thingy yay

--- 3.0Capstone/Assets/Scripts2.0/Managers/QVariants/QVariant1Manager.cs
+++ 3.0Capstone/Assets/Scripts2.0/Managers/QVariants/QVariant1Manager.cs
@@ -3,12 +3,21 @@ using UnityEngine;
 public class QVariant1Manager : MonoBehaviour
 {
     private bool buttonPressed = false;
-    
+    private AudioSource audioSource;
+    [SerializeField] private ParticleSystem crystalShards;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void Option1Press()
     {
         if (buttonPressed) return;
 
         Debug.Log("LEFT BUTTON PRESSED");
+        audioSource.Play();
+        crystalShards.Play();
         buttonPressed = true;
         GameManager.Instance.AddShards(50);
         GameflowManager flow = FindFirstObjectByType<GameflowManager>();
