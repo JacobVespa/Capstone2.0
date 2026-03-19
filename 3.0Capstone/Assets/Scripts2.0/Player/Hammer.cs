ce43a5d59ef3010fab91ce1d3b7eb26100985a54
Picklerick5028 <kostaskamniotis00755640@gmail.com>
2026-03-19 17:45:20 -0400

    Fixed

--- 3.0Capstone/Assets/Scripts2.0/Player/Hammer.cs
+++ 3.0Capstone/Assets/Scripts2.0/Player/Hammer.cs
@@ -28,6 +28,13 @@ public class Hammer : MonoBehaviour
     private bool shockwavePoweredUp;
     private bool pulseOnCooldown;
 
+    public bool isPoweredUp => shockwavePoweredUp;
+
+    public void CallStupidPulse()
+    {
+        TriggerShockwave();
+    }
+
     private void Start()
     {
         currentDamage = GetComponent<DamageSource>();
