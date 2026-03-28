e1463a007542d3c6ffc350ef547ce7f6cada3755
Absurd24 <160165230+Absurd24@users.noreply.github.com>
2026-03-28 03:08:47 -0400

    started leaderboard

---
+++ 3.0Capstone/Assets/Scripts2.0/AaronLeaderboardScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.IO;
+using TMPro;
+using System.Runtime.CompilerServices;
+using UnityEditor.Build.Player;
+
+public class AaronLeaderboardScript : MonoBehaviour
+{
+    void CreateLeaderboard()
+    {
+        string path = Application.dataPath + "/LocalLeaderboard.txt";
+
+        if (!File.Exists(path))
+        {
+            File.WriteAllText(path, "Highscores \n\n");
+        }
+
+        string content = "best score wow";
+
+        File.AppendAllText(path, content);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        CreateLeaderboard();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
