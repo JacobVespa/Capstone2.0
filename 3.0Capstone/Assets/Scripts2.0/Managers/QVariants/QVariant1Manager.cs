cfcd51099187d61b2f48b69677c5d644cc2518bd
Eliogeno <eliogbp@gmail.com>
2026-04-01 14:09:02 -0400

    Mystery level change yay

--- 3.0Capstone/Assets/Scripts2.0/Managers/QVariants/QVariant1Manager.cs
+++ 3.0Capstone/Assets/Scripts2.0/Managers/QVariants/QVariant1Manager.cs
@@ -11,19 +11,19 @@ public class QVariant1Manager : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
     }
 
-    public void Option1Press()
-    {
-        if (buttonPressed) return;
-
-        Debug.Log("LEFT BUTTON PRESSED");
-        audioSource.Play();
-        crystalShards.Play();
-        buttonPressed = true;
-        GameManager.Instance.AddShards(50);
-        GameflowManager flow = FindFirstObjectByType<GameflowManager>();
-        flow.LevelRunning = false;
-        GameManager.Instance.Victory();
-    }
+    //public void Option1Press()
+    //{
+    //    if (buttonPressed) return;
+    //
+    //    Debug.Log("LEFT BUTTON PRESSED");
+    //    audioSource.Play();
+    //    crystalShards.Play();
+    //    buttonPressed = true;
+    //    GameManager.Instance.AddShards(50);
+    //    GameflowManager flow = FindFirstObjectByType<GameflowManager>();
+    //    flow.LevelRunning = false;
+    //    GameManager.Instance.Victory();
+    //}
 
     public void Option2Press()
     {
